a257622af035fc8e6b3fcf0b40789b5489a4cd10
Nathan <thaonk26@uww.edu>
2016-09-12 18:15:15 -0500

    added more options on register

--- MunicipalTrashProgram/Models/AccountViewModels.cs
+++ MunicipalTrashProgram/Models/AccountViewModels.cs
@@ -65,6 +65,16 @@ namespace MunicipalTrashProgram.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "First Name")]
+        [DataType(DataType.Text)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [DataType(DataType.Text)]
+        public string LastName { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
