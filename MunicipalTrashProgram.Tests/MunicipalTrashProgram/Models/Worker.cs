5bac8a628a949c945fb4ba58bcc8b69a1f27b047
krisfalk <krisfalk2007@hotmail.com>
2016-09-13 08:31:35 -0500

    upload functional database

---
+++ MunicipalTrashProgram.Tests/MunicipalTrashProgram/Models/Worker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MunicipalTrashProgram.Models
+{
+    public class Worker
+    {
+        public Worker()
+        {
+
+        }
+        [Key]
+        public int Worker_id { get; set; }
+        //[ForeignKey("Person")]
+        //public int Person_id { get; set; }
+        //public Person person { get; set; }
+        public int WorkingZipCode { get; set; }
+    }
+}
